1733d1b172ea4fcaad4d5567f1c4764b1c483ba0
Daniel Dubas <dannydubas@unomaha.edu>
2023-04-07 12:39:38 -0500

    Added New Recipe Page for frontend

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import './Navbar.css'
-
+import NewRecipeForm from "../pages/NewRecipePage";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import RecipeMainPage from "../pages/RecipeMainPage";
+import './Navbar.css';
 const Navbar = () => {
-  return (
-      <ul>
-          <li><a className="#recipe" href="#home">Recipes</a></li>
-          <li><a href="#new">New Recipe</a></li>
-          <li className="floatRight"><a href="#signup">Signup</a></li>
-          <li className="floatRight"><a href="#login">Login</a></li>
-      </ul>
-  );
+    return (
+        <Router>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/" >Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/newPage" >New Recipe</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<RecipeMainPage />} />
+                <Route path="/newPage" element={<NewRecipeForm />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default Navbar;
--- my-app/src/pages/NewRecipePage.js
+++ my-app/src/pages/NewRecipePage.js
@@ -1,10 +1,58 @@
-import React from 'react';
-import './Navbar.css'
+import React, { useState } from 'react';
 
-const NewRecipe = () => {
+function NewRecipeForm() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [ingredients, setIngredients] = useState('');
+    const [instructions, setInstructions] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Send data to backend or perform any other desired action
+        console.log(title, description, ingredients, instructions);
+    };
+
     return (
-        <h1>Hi I'm the new recipe page</h1>
-    );
-};
 
-export default NewRecipe;
+        <form onSubmit={handleSubmit}>
+            <h1>Add New Recipe</h1>
+            <div>
+                <label htmlFor="title">Recipe Title:</label>
+                <input
+                    type="text"
+                    id="title"
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="description">Description:</label>
+                <textarea
+                    id="description"
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
+                ></textarea>
+            </div>
+            <div>
+                <label htmlFor="ingredients">Ingredients:</label>
+                <textarea
+                    id="ingredients"
+                    value={ingredients}
+                    onChange={(event) => setIngredients(event.target.value)}
+                ></textarea>
+            </div>
+            <div>
+                <label htmlFor="instructions">Instructions:</label>
+                <textarea
+                    id="instructions"
+                    value={instructions}
+                    onChange={(event) => setInstructions(event.target.value)}
+                ></textarea>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
+
+export default NewRecipeForm;
